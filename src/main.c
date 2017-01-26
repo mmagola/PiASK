ae641fa88e6adabc4afed3d4718586acc5a08a34
mmagola <mmagola@mailplus.pl>
2017-01-27 00:35:31 +0100

    histogram tylko dla skali szarości. dziala poprawnie

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
  Author      : MIS
  Version     :
  Copyright   : Your copyright notice
- Description : Hello OpenMP World in C
+ Description : Only for image in grayscale
  ============================================================================
  */
 #include <omp.h>
@@ -15,39 +15,64 @@
 #include "FreeImage.h"
 
 #define NBR_OF_ELEMENTS 40000
+#define hist_size 256
+#define th_num 4
 
 main() {
-
-
+	omp_set_num_threads(th_num);
+	int hist_table[hist_size] = { 0 };
+	int bytespp;
 	FreeImage_Initialise(TRUE);
-	FIBITMAP *bitmap = FreeImage_Load(FIF_BMP, "C:\\Studia\\IX_Semestr\\PIASK\\part_2\\project\\eclipse_Mars_worlspace\\lena.bmp", BMP_DEFAULT);
-	if(bitmap){
+	int check = 0;
+	FIBITMAP *bitmap = FreeImage_Load(FIF_JPEG, "lenna512x512_gray.jpg",JPEG_DEFAULT);
+	if (bitmap)
+		{
 		unsigned width = FreeImage_GetWidth(bitmap);
 		unsigned height = FreeImage_GetHeight(bitmap);
 		unsigned pitch = FreeImage_GetPitch(bitmap);
 
-		printf("obrazek zaladowany \n width = %d, height = %d, pitch = %d \n", width, height,pitch );
+		printf("picture loaded \n width = %d, height = %d, pitch = %d \n", width, height, pitch);
+
+		// Calculate the number of bytes per pixel (3 for 24-bit or 4 for 32-bit)
+		bytespp = FreeImage_GetLine(bitmap) / width;
+		printf(" bytes per pixel = %d \n ", bytespp);
+
 		BYTE *ptr = FreeImage_GetBits(bitmap);
-		if(ptr){
-			printf("udalo sie tez GetBits\n");
-		}
-		FreeImage_Unload(bitmap);
-	}else{
-		printf("obrazek nie za³adowany\n bitmap = %d", bitmap);
+		if (ptr)
+			{
+			printf("GetBits_works\n");
+
+			start_time();
+			///****************  parallel section ***********************//////
+			#pragma omp parallel
+			{
+				int i, hist_priv[hist_size] = {0};
+				#pragma omp for nowait
+					for(i=0;i<height*width;i++)
+					{
+						hist_priv[ptr[i]]++;
+					}
+				#pragma omp critical
+				{
+					for(i=0; i<hist_size;i++) {
+						hist_table[i] += hist_priv[i];
+					}
+				}
+			}
+			stop_time();
+			for (int i = 0; i < hist_size; i++){
+				printf("%d ", hist_table[i]);
+				check += hist_table[i];
+			}
+			printf("\ncheck = %d, nummber of pixel = %d ", check, width*height);
+			print_time("\nElapsed:");
+
+
+			}
+			FreeImage_Unload(bitmap);
+	} else{
+		printf("picture not loaded \n ");
 	}
-
-
-	long i = 0;
-	float a[NBR_OF_ELEMENTS];
-	omp_set_num_threads(4);
-	start_time();
-#pragma omp parallel for
-	for (i = 0; i < NBR_OF_ELEMENTS; i++)
-		a[i] = sqrt(i);
-
-	stop_time();
-	print_time("Elapsed:");
-	//FreeImage_Unload(bitmap);
 	FreeImage_DeInitialise();
 }
 
